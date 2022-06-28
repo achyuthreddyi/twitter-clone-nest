56faab1daa02b663990548177d1e0c9a849aad9c
Achyuth Reddy <ncsachyuth@gmail.com>
2022-06-28 12:42:34 +0530

    Implemented the validation pipes to validate tweet dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TweetModule } from './tweet/tweet.module';
 
 @Module({
-  imports: [],
+  imports: [TweetModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/pipes/validation.pipe.ts
@@ -0,0 +1,23 @@
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      throw new BadRequestException('data passed is not in the required format');
+    }
+    return value;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
